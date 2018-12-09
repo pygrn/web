4c647aa823e327fb3a30e37b61c3002bda959b4d
Xavi Torelló <info@xaviertorello.cat>
2018-12-09 04:02:17 +0100

    Add new component IssuesListItem

---
+++ src/.vuepress/components/IssuesListItem.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+  <h2>
+      <a target="_blank" :href="html_url" >{{ title }}</a>
+  </h2>
+
+  <p v-html="markdown(body)"></p>
+
+  <p><a target="_blank" :href="html_url">Més informació</a></p>
+</div>
+</template>
+
+<script>
+const MarkdownIt = require('markdown-it'),
+      md = new MarkdownIt();
+
+export default {
+    name: "IssuesListItem",
+
+    data() {
+      return {
+      }
+    },
+
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+      body: {
+        type: String,
+        required: true,
+      },
+      html_url: {
+        type: String,
+        required: true,
+      },
+    },
+
+    methods: {
+      markdown: function (content) {
+        return md.render(content);
+      }
+    },
+}
+</script>
